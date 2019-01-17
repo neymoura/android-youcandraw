feb6e900f704919281fae59695de1d8408424cdc
Ney Moura <ney.moura@gmail.com>
2019-01-17 17:31:23 -0200

    Creating instrumented test

--- app/src/main/java/com/ioraptor/youcandraw/MainActivity.kt
+++ app/src/main/java/com/ioraptor/youcandraw/MainActivity.kt
@@ -2,8 +2,8 @@ package com.ioraptor.youcandraw
 
 import android.graphics.*
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
